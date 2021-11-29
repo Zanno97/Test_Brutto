964a5c4f6fbd32990adad04f9cf63b064847dee4
Luca Zanolini <lucazanolini@Lucas-MacBook-Pro.local>
2021-11-29 13:02:27 +0100

    back done front shit

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <navigator/>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import navigator from './components/navigator.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    navigator,
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
